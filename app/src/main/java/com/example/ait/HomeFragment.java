2d915c4ea3f0cb6e61b5c6ee748249889fe32457
m4xvel <mmenshov2004@gmail.com>
2023-09-10 19:41:49 +0300

    "Changed design HomeFragment"

--- app/src/main/java/com/example/ait/HomeFragment.java
+++ app/src/main/java/com/example/ait/HomeFragment.java
@@ -1,12 +1,16 @@
 package com.example.ait;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class HomeFragment extends Fragment {
 
@@ -16,6 +20,7 @@ public class HomeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.home_fragment, container, false);
 
         requireActivity().getWindow().setStatusBarColor(getResources().getColor(R.color.gray, requireActivity().getTheme()));
+
         return rootView;
     }
 }
